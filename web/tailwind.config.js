cc3be4f4b77aa573513a49a13c279bd02bd67929
Vishwas Pandey <Vishwaspandey440@gmail.com>
2026-01-15 15:04:11 +0530

    made design changes

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,19 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  // 👇 THIS FORCES THE STYLES TO EXIST IN PRODUCTION
-  safelist: [
-    "bg-black",
-    "text-white",
-    "border",
-    "border-gray-300",
-    "rounded-xl",
-    "p-8",
-    "shadow-xl",
-    "hover:bg-gray-800",
-    "focus:ring-black",
-    "w-full",
-  ],
   theme: {
     extend: {},
   },
